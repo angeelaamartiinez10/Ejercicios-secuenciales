8bfa2cbe1c3015ecde312e9458979d1d86df3024
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-09-30 11:12:26 +0200

    Ejercicios 1 y 2 terminados

---
+++ Ejercicio 1.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+nombre=""
+nombre=input("¿Cúal es tu nombre? ")
+print("tu nombre es:" ,nombre,)
---
+++ Ejercicio 2.py
@@ -0,0 +1,7 @@
+#Calcular el perímetro y área de un rectángulo dada su base y su altura.
+base=0
+altura=0
+
+base=(int)(input("¿Cúanto mide la base? "))
+altura=(int)(input("¿Cúanto mide la altura? "))
+print("El área es: " ,base * altura, )
