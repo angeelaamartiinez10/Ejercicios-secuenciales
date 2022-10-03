cebf9c17629a6136857b4cc177e5ad830800d75c
ángela <al_amartinezn@iessierradelasvillas.org>
2022-10-03 22:03:16 +0200

    revision ejercicios

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -3,8 +3,8 @@ num1 = 0
 num2 = 0
 num3 = 0
 
-num1=(int)(input("Dime el primer numero"))
-num2=(int)(input("Dime el segundo numero"))
-num3=(int)(input("Dime el tercer numero"))
+num1=(int)(input("Dime el primer numero "))
+num2=(int)(input("Dime el segundo numero "))
+num3=(int)(input("Dime el tercer numero "))
 
 print("La media de tus numeros" ,(num1+num2+num3)/3,)
---
+++ Ejercicio 9.py
@@ -0,0 +1,7 @@
+#Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente
+#desea saber cuanto deberá pagar finalmente por su compra.
+
+precio = 0
+
+precio=(int)(input("¿Cuánto llevas gastado?"))
+print("El precio total con el descuento del 15%= ",precio-precio*0.15," €")
