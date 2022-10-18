49ed8a0b475dabab91afd854f2f2358f625b9849
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-18 14:41:16 +0200

    ejercicios funciones

---
+++ EjercicioS funcionES.py
@@ -0,0 +1,17 @@
+#FUNCION
+#Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+
+
+def ejer1():
+    nombre=""
+    nombre=input("¿Cúal es tu nombre? ")
+    print("tu nombre es:" ,nombre,)
+
+
+def ejer2(base and altura):
+    base=0
+    altura=0
+
+base=(int)(input("¿Cúanto mide la base? "))
+altura=(int)(input("¿Cúanto mide la altura? "))
+print("El área es: " ,base * altura, )
